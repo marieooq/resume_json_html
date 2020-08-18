b4d2a333619f2f5684abe0be6fdc522d886b1c97
Marie Otaki <otakimarie@gmail.com>
2020-08-17 23:50:58 -0700

    read the json file in json_to_html.js

---
+++ js/json_to_html.js
@@ -0,0 +1,17 @@
+$(document).ready(function () {
+  $.getJSON('../resume.json', function (data) {
+    // for (var i in data) {
+    //   $('#output').append('<li><strong>' + data[i].division + '</strong></li>');
+    //   for (var j in data[i].person) {
+    //     $('#output').append(
+    //       '<li>' +
+    //         data[i].person[j].name +
+    //         '（' +
+    //         data[i].person[j].age +
+    //         '才）</li>n'
+    //     );
+    //   }
+    // }
+      console.log(data);
+  });
+});

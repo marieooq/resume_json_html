848573f09f1cd725d2f954631e252cb9f13eb170
Marie Otaki <otakimarie@gmail.com>
2020-08-18 15:39:34 -0700

    change the argument name for each function so you can understand the meaning easily

--- js/json_to_html.js
+++ js/json_to_html.js
@@ -1,26 +1,26 @@
-function displayTitle (data)
+function displayTitle (title)
 {
   //insert a title
-  if (data.title) {
-    $('#title').append(data.title);
+  if (title) {
+    $('#title').append(title);
   } else
   {
       return;
   }
 }
 
-function displayContactSection (data)
+function displayContactSection (contact)
 {
-    if (data.contact) {
+    if (contact) {
       const left = $('<div>', { class: 'contact_left' });
       const left_ul = $('<ul>', { class: 'contact_ul' });
 
       const right = $('<div>', { class: 'contact_right' });
       const right_ul = $('<ul>', { class: 'contact_ul' });
 
-      for (let i = 0; i < data.contact.length; i++) {
-        const title = data.contact[i].item_title;
-        const content = data.contact[i].item_content;
+      for (let i = 0; i < contact.length; i++) {
+        const title = contact[i].item_title;
+        const content = contact[i].item_content;
 
           const li = $('<li>', { text: title + ' - ' });
           
@@ -54,125 +54,117 @@ function displayContactSection (data)
     }
 }
 
-function displaySummarySection (data)
-{
-    if (data.summary) {
-      const title = data.summary.title;
-      const content = data.summary.content;
-      const h2 = $('<h2>', { text: title });
-      const p = $('<p>', { text: content });
-      $('.summary').append(h2, p);
-    } else
-    {
-        return;
-    }
+function displaySummarySection(summary) {
+  if (summary) {
+    const title = summary.title;
+    const content = summary.content;
+    const h2 = $('<h2>', { text: title });
+    const p = $('<p>', { text: content });
+    $('.summary').append(h2, p);
+  } else {
+    return;
+  }
 }
 
-function displayWorkExperienceSection (data)
-{
-    if (data.work_experience) {
-      const title = data.work_experience.title;
-      const h2 = $('<h2>', { text: title });
-      $('.work_experience').append(h2);
+function displayWorkExperienceSection(work_experience) {
+  if (work_experience) {
+    const title = work_experience.title;
+    const h2 = $('<h2>', { text: title });
+    $('.work_experience').append(h2);
 
-      const careers = data.work_experience.career;
+    const careers = work_experience.career;
 
-      for (let i = 0; i < careers.length; i++) {
-        const div = $('<div>', { class: 'career' });
+    for (let i = 0; i < careers.length; i++) {
+      const div = $('<div>', { class: 'career' });
 
-        if (careers[i].title) {
-          const job_title = careers[i].title;
-          const h3 = $('<h3>', { text: job_title });
-          div.append(h3);
-        }
-        if (careers[i].company_description) {
-          const company_description = careers[i].company_description;
-          const h4 = $('<h4>', { text: company_description });
-          div.append(h4);
-        }
-
-        if (careers[i].achievements) {
-          const ul = $('<ul>', { class: 'career_ul' });
-          const achievements = careers[i].achievements;
-
-          for (let j = 0; j < achievements.length; j++) {
-            const achievement = achievements[j];
-            const li = $('<li>', { text: achievement });
-            ul.append(li);
-          }
-          div.append(ul);
-        }
-
-        $('.work_experience').append(div);
+      if (careers[i].title) {
+        const job_title = careers[i].title;
+        const h3 = $('<h3>', { text: job_title });
+        div.append(h3);
       }
-    } else
-    {
-        return;
+      if (careers[i].company_description) {
+        const company_description = careers[i].company_description;
+        const h4 = $('<h4>', { text: company_description });
+        div.append(h4);
+      }
+
+      if (careers[i].achievements) {
+        const ul = $('<ul>', { class: 'career_ul' });
+        const achievements = careers[i].achievements;
+
+        for (let j = 0; j < achievements.length; j++) {
+          const achievement = achievements[j];
+          const li = $('<li>', { text: achievement });
+          ul.append(li);
+        }
+        div.append(ul);
+      }
+
+      $('.work_experience').append(div);
     }
+  } else {
+    return;
+  }
 }
 
-function displayProjectsSection (data)
-{
-    if (data.projects) {
-      if (data.projects.title) {
-        const title = data.projects.title;
-        const h2 = $('<h2>', { text: title });
-        $('.projects').append(h2);
-      }
-
-      if (data.projects.project_list) {
-        const project_list = data.projects.project_list;
-        for (let i = 0; i < project_list.length; i++) {
-          const project = $('<div>', { class: 'project' });
-          const project_name = project_list[i].project_name;
-          const project_url = project_list[i].project_url;
-          const description = project_list[i].description;
-
-          const h3 = $('<h3>', { text: project_name });
-
-          const span = $('<span>');
-          const a = $('<a>', {
-            text: ' ' + project_url,
-            href: project_url,
-            target: '_blank',
-          });
-
-          const p = $('<p>', { text: description });
-
-          span.append(a);
-          h3.append(span);
-          project.append(h3, p);
-          $('.projects').append(project);
-        }
-      }
-    } else
-    {
-        return;
+function displayProjectsSection(projects) {
+  if (projects) {
+    if (projects.title) {
+      const title = projects.title;
+      const h2 = $('<h2>', { text: title });
+      $('.projects').append(h2);
     }
+
+    if (projects.project_list) {
+      const project_list = projects.project_list;
+      for (let i = 0; i < project_list.length; i++) {
+        const project = $('<div>', { class: 'project' });
+        const project_name = project_list[i].project_name;
+        const project_url = project_list[i].project_url;
+        const description = project_list[i].description;
+
+        const h3 = $('<h3>', { text: project_name });
+
+        const span = $('<span>');
+        const a = $('<a>', {
+          text: ' ' + project_url,
+          href: project_url,
+          target: '_blank',
+        });
+
+        const p = $('<p>', { text: description });
+
+        span.append(a);
+        h3.append(span);
+        project.append(h3, p);
+        $('.projects').append(project);
+      }
+    }
+  } else {
+    return;
+  }
 }
 
 
-function displayEducationSection (data)
-{
-    if (data.education) {
-      const title = data.education.title;
-      const h2 = $('<h2>', { text: title });
-      $('.education').append(h2);
+function displayEducationSection(education) {
+  if (education) {
+    const title = education.title;
+    const h2 = $('<h2>', { text: title });
+    $('.education').append(h2);
 
-      const educational_background = data.education.educational_background;
+    const educational_background = education.educational_background;
 
-      for (let i = 0; i < educational_background.length; i++) {
-        const major = educational_background[i].major;
-        const description = ' ' + educational_background[i].description;
-        const h3 = $('<h3>');
-        const span = $('<span>', { text: major });
-        h3.append(span, description);
-        $('.education').append(h3);
-      }
-    } else
-    {
-        return;
+    for (let i = 0; i < educational_background.length; i++) {
+      const major = educational_background[i].major;
+      const description = ' ' + educational_background[i].description;
+      const h3 = $('<h3>');
+      const span = $('<span>', { text: major });
+      h3.append(span, description);
+      $('.education').append(h3);
     }
+  } else {
+    return;
+  }
 }
 
 
@@ -180,23 +172,22 @@ $(document).ready(function ()
 {
     $.getJSON('../resume.json', function (data)
     {
-
         //display a title
-        displayTitle(data);
+        displayTitle(data.title);
 
         // display a contact section
-        displayContactSection(data);
+        displayContactSection(data.contact);
 
         //display a summary section
-        displaySummarySection(data);
+        displaySummarySection(data.summary);
 
         //display a work experience section
-        displayWorkExperienceSection (data);
+        displayWorkExperienceSection(data.work_experience);
 
         //display a projects section
-        displayProjectsSection(data);
+        displayProjectsSection(data.projects);
 
         //display an education section
-        displayEducationSection(data);
+        displayEducationSection(data.education);
     });
 });
